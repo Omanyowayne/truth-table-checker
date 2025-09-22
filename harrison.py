373182054568f2b2d7ebb011cc895f454722a022
Omanyowayne <omanyowayne@gmail.com>
2025-09-22 10:57:01 +0300

    Update harrison.py

--- harrison.py
+++ harrison.py
@@ -1,1 +1,14 @@
+from logic import *
 
+rain = Symbol("rain")
+hagrid = Symbol("Hagrid")
+dumbledore = Symbol("Dumbledore")
+
+knowledge_base = And(
+    Implication(Not(rain), hagrid),
+    Or(hagrid, dumbledore),
+    Not(And(hagrid, dumbledore)),
+    hagrid
+)
+
+print(model_check(knowledge_base, hagrid))
